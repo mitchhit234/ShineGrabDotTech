36d906cc54d15388134d8c03ff0dedc51b5b46c9
Jack <jhkmw3@mst.edu>
2021-04-09 21:56:18 -0500

    Fix game file

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const { default: SlippiGame } = require('@slippi/slippi-js');
 const game = new SlippiGame("test_game.slp");
 
 const frames = game.getFrames()
-console.log(frames[0].players)
+var i;
+for(i=0; i < 1000; i++) {
+  console.log(frames[i].players[0]['post'])
+}
