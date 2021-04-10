234673135d3745ecef3ee2cdac2d1b1a21b7edc0
Jack Kufa <jack@kufa.io>
2021-04-10 02:13:09 -0500

    Ooga booga shine grab code

--- script.js
+++ script.js
@@ -6,23 +6,41 @@
 // getMetadata(): MetadataType;
 // getFilePath(): string | null;
 
+const SHINE = 361;
+const PRECUM = 24; // prejump ;)
+const GRAB = 212;
+
 const { default: SlippiGame } = require('@slippi/slippi-js');
-const game = new SlippiGame("test_game.slp");
+const game = new SlippiGame("shinegrab.slp");
 
 const frames = game.getFrames()
 const meta_data = game.getMetadata();
 var i;
+var j;
+var k;
 
-console.log(frames[836].players[1]['post']['actionStateId'])
-
-
-
-for(i=0; i < meta_data.length; i++) {
-  //console.log(frames[0].players[1]['post'])
-  if(frames[i].players[1]['post']['actionStateId'] == 361){
-    //console.log(frames[836].players[1]['post']['actionStateId'])
-    //console.log(i + " : frame")
-    console.log(i +", " + frames[i].players[1]['post']['actionStateId'])
+for(i=-123; i < meta_data['lastFrame']; i++) {
+  var is_shinegrab = false
+  if(frames[i].players[0]['post']['actionStateId'] == SHINE){
+    // console.log(i +", " + frames[i].players[0]['post']['actionStateId'])
+    // 21 frames after i is acceptable for a shine grab
+    for(j = i; j < i+21; j++) {
+      // check if action state is not shine
+      if(frames[j].players[0]['post']['actionStateId'] != SHINE){
+        var new_action = frames[j].players[0]['post']['actionStateId']
+        if(new_action == PRECUM) {
+          // check next three frames for grab
+          for(k = j+1; k <= j+3; k++) {
+            if(frames[k].players[0]['post']['actionStateId'] == GRAB) {
+              is_shinegrab = true
+              i = i+4
+            }
+          }
+        }
+      }
+    }
+    if(is_shinegrab) {
+      console.log("Shine grab")
+    }
   }
-  //console.log(frames[i].players[1]['post']['lastAttackLanded'])
 }
