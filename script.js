9c08f96c871582cf69c19e815b08588253df6b26
Mitchell Meier <mdmfvz@mst.edu>
2021-04-10 00:13:50 -0500

    length function found

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// getSettings(): GameStartType;
+// getLatestFrame(): FrameEntryType | null;
+// getGameEnd(): GameEndType | null;
+// getFrames(): FramesType;
+// getStats(): StatsType;
+// getMetadata(): MetadataType;
+// getFilePath(): string | null;
+
 const { default: SlippiGame } = require('@slippi/slippi-js');
 const game = new SlippiGame("test_game.slp");
 
 const frames = game.getFrames()
+const meta_data = game.getMetadata();
 var i;
 
 console.log(frames[836].players[1]['post']['actionStateId'])
 
-for(i=0; i < 15021; i++) {
+
+
+for(i=0; i < meta_data.length; i++) {
   //console.log(frames[0].players[1]['post'])
   if(frames[i].players[1]['post']['actionStateId'] == 361){
     //console.log(frames[836].players[1]['post']['actionStateId'])
