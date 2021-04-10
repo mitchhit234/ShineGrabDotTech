0f7f7e53869ed5b3dd59f90bff62185e9b338b28
RamiHanin07 <69398333+RamiHanin07@users.noreply.github.com>
2021-04-10 00:02:48 -0500

    Shine and Frame Count

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const game = new SlippiGame("test_game.slp");
 
 const frames = game.getFrames()
 var i;
-for(i=0; i < 1000; i++) {
-  console.log(frames[i].players[0]['post'])
+
+console.log(frames[836].players[1]['post']['actionStateId'])
+
+for(i=0; i < 15021; i++) {
+  //console.log(frames[0].players[1]['post'])
+  if(frames[i].players[1]['post']['actionStateId'] == 361){
+    //console.log(frames[836].players[1]['post']['actionStateId'])
+    //console.log(i + " : frame")
+    console.log(i +", " + frames[i].players[1]['post']['actionStateId'])
+  }
+  //console.log(frames[i].players[1]['post']['lastAttackLanded'])
 }
